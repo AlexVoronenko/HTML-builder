322b2951c86fc7d02485adb673d6f54573f032d9
Alexander Voronenko <796546@gmail.com>
2022-11-02 08:01:56 +0300

    feat: tast 01-read-file done

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+
+const filePath = path.join(__dirname,"text.txt");
+stdout.write("Полный путь для файла:"+"\n");
+stdout.write(filePath+"\n");
+
+let readableStream = fs.createReadStream( filePath,'utf8' );
+
+readableStream.on("data", function(chunk){
+    console.log(chunk);
+});
