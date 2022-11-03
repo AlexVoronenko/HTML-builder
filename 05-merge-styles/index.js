ce96455e692db22fd6c29d7a8ee27e8b5acb42e3
Alexander Voronenko <796546@gmail.com>
2022-11-03 10:49:35 +0300

    feat:fix error in 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -10,14 +10,14 @@ async function mergeCss (src,dst) {
 try {
    const files = await fs.readdir(src, { withFileTypes: true },);
    for (const file of files)  {
-    console.log("step5");
      if(file.isFile()){
         if(path.extname(file.name)===".css"){
-            const data = await fs.readFile(path.join(src,path.parse(file.name).base,));
+           const data = await fs.readFile(path.join(src,file.name,));
             await fs.appendFile(path.join(dst,"bundle.css"),data);    
         }
       } 
     }
+    console.log(`File creation completed successfully (bundle.css)`);
 } catch (error) {
     console.log(`Error: ${error}. Can not sync dirert.`);
 }
